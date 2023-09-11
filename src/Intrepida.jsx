8b7034a83a31e94cd0cea1b1b8fd1e6e9dac81b0
Juan Alvarez <juan.alvarez.v93@gmail.com>
2023-09-11 16:45:27 -0300

    first commit

---
+++ src/Intrepida.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './style.css'
+
+function Intrepida() {
+  return (
+    <>
+      <h1>
+        hola
+      </h1>
+    </>
+  )
+}
+
+export default Intrepida
