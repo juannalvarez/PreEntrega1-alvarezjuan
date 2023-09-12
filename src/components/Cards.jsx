da764ce8d6ea078d71a07cd69889a5770df6028f
Juan Alvarez <juan.alvarez.v93@gmail.com>
2023-09-12 01:29:51 -0300

    Segui trabajando en las cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,9 +7,9 @@ function Cards() {
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="./src/assets/img/vino-rutini.jpg" />
                 <Card.Body>
-                    <Card.Title>Vino De Sangre</Card.Title>
+                    <Card.Title>Bodega Rutini</Card.Title>
                     <Card.Text>
-                        <h3>Bodega Luigi Bosca</h3>
+                        <h3>Vino 1</h3>
                         <p>Descripcion</p>
                         <strong>$10.000</strong>
                     </Card.Text>
@@ -19,11 +19,11 @@ function Cards() {
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="./src/assets/img/vino-luigibosca.png" />
                 <Card.Body>
-                    <Card.Title>Vino De Sangre</Card.Title>
+                    <Card.Title>Bodega Luigi Bosca</Card.Title>
                     <Card.Text>
-                        <h3>Bodega Luigi Bosca</h3>
+                        <h3>Vino 2</h3>
                         <p>Descripcion</p>
-                        <strong>$10.000</strong>
+                        <strong>$12.000</strong>
                     </Card.Text>
                     <Button variant="primary">Agregar al carrito</Button>
                 </Card.Body>
@@ -31,11 +31,11 @@ function Cards() {
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="./src/assets/img/vino-loshelechos.jpg" />
                 <Card.Body>
-                    <Card.Title>Vino De Sangre</Card.Title>
+                    <Card.Title>Bodega Los Helechos</Card.Title>
                     <Card.Text>
-                        <h3>Bodega Luigi Bosca</h3>
+                        <h3>Vino 3</h3>
                         <p>Descripcion</p>
-                        <strong>$10.000</strong>
+                        <strong>$8.000</strong>
                     </Card.Text>
                     <Button variant="primary">Agregar al carrito</Button>
                 </Card.Body>
